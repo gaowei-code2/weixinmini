30dbdbe450907c251cfa8dad87865aa3b3194640
gaowei-code2 <gw13282190089@163.com>
2021-04-09 17:12:12 +0800

    “”

---
+++ src/main/java/com/gaowei/weixinmini/WeixinminiApplication.java
@@ -0,0 +1,13 @@
+package com.gaowei.weixinmini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinminiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeixinminiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaowei/weixinmini/WeixinminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaowei.weixinmini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeixinminiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
